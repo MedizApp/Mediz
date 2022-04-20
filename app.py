c3bf0f36cce99c477cc08d923a1312757b633315
Hardik Jade <hardikjade2808@gmail.com>
2022-04-21 01:08:25 +0530

    Final Commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, flash, redirect
+import pickle
+import numpy as np
+from tensorflow.keras.models import load_model
+app = Flask(__name__)
+def predict(values, dic):
+    if len(values) == 13:
+        model = pickle.load(open('models/heart.pkl','rb'))
+        values = np.asarray(values)
+        return model.predict(values.reshape(1, -1))[0]
+    elif len(values) == 18:
+        model = pickle.load(open('models/kidney.pkl','rb'))
+        values = np.asarray(values)
+        return model.predict(values.reshape(1, -1))[0]
+    elif len(values) == 10:
+        model = pickle.load(open('models/liver.pkl','rb'))
+        values = np.asarray(values)
+        return model.predict(values.reshape(1, -1))[0]
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/heart", methods=['GET', 'POST'])
+def heartPage():
+    return render_template('heart.html')
+
+@app.route("/kidney", methods=['GET', 'POST'])
+def kidneyPage():
+    return render_template('kidney.html')
+
+@app.route("/liver", methods=['GET', 'POST'])
+def liverPage():
+    return render_template('liver.html')
+
+@app.route("/predict", methods = ['POST', 'GET'])
+def predictPage():
+    try:
+        if request.method == 'POST':
+            to_predict_dict = request.form.to_dict()
+            to_predict_list = list(map(float, list(to_predict_dict.values())))
+            pred = predict(to_predict_list, to_predict_dict)
+    except:
+        message = "Please Enter Valid Data"
+        return render_template("home.html", message = message)
+    return render_template('predict.html', pred = pred)
+    
+
+if __name__ == '__main__':
+	app.run(debug = True)
